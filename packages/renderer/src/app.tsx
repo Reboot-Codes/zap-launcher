42526cddfc2d3cf4ed3a40ed8e665c138d766964
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-11-09 16:13:15 -0700

    Added "Real" Search Functionality

--- packages/renderer/src/app.tsx
+++ packages/renderer/src/app.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import './app.scss';
-
-const results = [
-  "Hello1",
-  "Hello2",
-  "Hello3",
-  "Hello4",
-  "Hello5",
-  "Hello6",
-  "Hello7",
-  "Hello8",
-  "Hello9",
-];
+import { files } from './use/test-array';
 
 export default function App() {
-  const [query, setQuery] = React.useState('')
+  const [results, setResults] = React.useState<string[]>([]);
+  const handleQuery = (query: string) => {
+    let localResults: string[] = [];
+    files.forEach(value => {
+      if (query.length > 0 && !localResults.includes(value) && localResults.length < 9) {
+        if (value.toLowerCase().includes(query.toLowerCase())) {
+          localResults.push(value);
+        }
+      }
+    });
+    setResults(localResults);
+  }
 
   return (
     <div className="app">
       <div className="vite-glass">
-        <input className="text-box" onChange={e => setQuery(e.target.value)} />
+        <input className="text-box" onChange={e => handleQuery(e.target.value)} />
         <div className="results">
           {results.map(result => {
             return (
-              <div className="result">
+              <div className="result" key={result}>
                 {result}
               </div>
             )
