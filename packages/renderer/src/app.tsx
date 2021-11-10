bb0f340d081f6987675129eb48e348a22607d4ec
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-11-10 15:47:37 -0700

    Add Special Options

--- packages/renderer/src/app.tsx
+++ packages/renderer/src/app.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import './app.scss';
 import { files } from './use/test-array';
+import { superOptions } from './use/super-options';
+
+import './app.scss';
 
 export default function App() {
   const [results, setResults] = React.useState<string[]>([]);
+
   const handleQuery = (query: string) => {
-    let localResults: string[] = [];
-    files.forEach(value => {
-      if (query.length > 0 && !localResults.includes(value) && localResults.length < 9) {
-        if (value.toLowerCase().includes(query.toLowerCase())) {
-          localResults.push(value);
+    const localResults: string[] = [];
+    const allOptions = superOptions.concat(files);
+    allOptions.forEach(option => {
+      // Make sure that there is a query, no repeats, and cap the results to 9 total.
+      if (query.length > 0 && !localResults.includes(option) && localResults.length < 9) {
+        // Check if the option contains the query
+        if (option.toLowerCase().includes(query.toLowerCase())) {
+          // if it does, then add the option to the results
+          localResults.push(option);
         }
       }
     });
+    // Set the finished results to display in the results list
     setResults(localResults);
-  }
+  };
 
   return (
     <div className="app">
@@ -26,7 +34,7 @@ export default function App() {
               <div className="result" key={result}>
                 {result}
               </div>
-            )
+            );
           })}
         </div>
       </div>
---
+++ packages/renderer/src/use/super-options.ts
@@ -0,0 +1,12 @@
+export const superOptions: string[] = [
+  'Screen Saver',
+  'Show Trash',
+  'Empty Trash',
+  'Logout',
+  'Sleep',
+  'Sleep Displays',
+  'Lock',
+  'Restart',
+  'Shutdown',
+  'Eject All'
+];
